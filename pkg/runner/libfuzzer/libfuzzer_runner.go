CodeIntelligenceTesting/cifuzz

commit 1cb08817f8a74c386324389a5f03bd2f1dc4343d

Use errors.As instead of type assertion
errors.As automatically unwraps the error for us, so we don't have to
call errors.Cause anymore on the error.

pkg/runner/libfuzzer/libfuzzer_runner.go
@@ -327,9 +327,10 @@ func IsExpectedExitError(err error) bool {
 		fuzzer_runner.LibFuzzerOOMExitCode,
 		fuzzer_runner.LibFuzzerTimeoutExitCode,
 	}
-	exitError, ok := errors.Cause(err).(*exec.ExitError)
-	if !ok {
+
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) {
 		return false
 	}
-	return sliceutil.Contains(expectedExitCodes, exitError.ExitCode())
+	return sliceutil.Contains(expectedExitCodes, exitErr.ExitCode())
 }

tools/cmake/cmake_test.go
@@ -13,6 +13,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -183,7 +184,8 @@ func runInDirWithExpectedStatus(t *testing.T, expectFailure bool, dir string, co
 	c := exec.CommandContext(ctx, command, args...)
 	c.Dir = dir
 	out, err := c.Output()
-	if exitErr, ok := err.(*exec.ExitError); ok {
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) {
 		msg := fmt.Sprintf("%q exited with %d:\nstderr:\n%s\nstdout:\n%s", c.String(), exitErr.ExitCode(), string(exitErr.Stderr), string(out))
 		if !expectFailure {
 			require.NoError(t, exitErr, msg)

tools/replayer/replayer_test.go
@@ -14,11 +14,12 @@ import (
 	"strings"
 	"testing"
 
-	"code-intelligence.com/cifuzz/util/fileutil"
-	"code-intelligence.com/cifuzz/util/testutil"
-
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"code-intelligence.com/cifuzz/util/fileutil"
+	"code-intelligence.com/cifuzz/util/testutil"
 )
 
 //go:embed src/replayer.c
@@ -380,7 +381,8 @@ func subtestCompileAndRunWithFuzzerInitialize(t *testing.T, cc compilerCase, rcs
 
 				stdoutLines, stderr, err := runReplayer(t, tempDir, replayer, rc.inputs...)
 				if rc.normalExit {
-					if exitErr, ok := err.(*exec.ExitError); ok {
+					var exitErr *exec.ExitError
+					if errors.As(err, &exitErr) {
 						require.NoError(t, err, string(exitErr.Stderr))
 					} else {
 						require.NoError(t, err)
@@ -418,7 +420,8 @@ func subtestCompileAndRunWithoutFuzzerInitialize(t *testing.T, cc compilerCase) 
 		)...)
 
 		stdoutLines, stderr, err := runReplayer(t, tempDir, replayer, "foo", "bar")
-		if exitErr, ok := err.(*exec.ExitError); ok {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
 			require.NoError(t, err, string(exitErr.Stderr))
 		} else {
 			require.NoError(t, err)
@@ -490,8 +493,10 @@ func outputWithStderr(cmd *exec.Cmd) (stdout []byte, stderr []byte, err error) {
 	stdout, err = cmd.Output()
 	stderr = stderrBuf.Bytes()
 	// Also store stderr in the ExitError in case of failure to remain compatible with cmd.Output.
-	if exitErr, ok := err.(*exec.ExitError); ok {
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) {
 		exitErr.Stderr = stderr
+		err = exitErr
 	}
 	return
 }

commit b043117e36f907aff27f5a0e052d14858b73aac4

Fix IsExpectedExitError
Commit 1cb08817f8a74c386324389a5f03bd2f1dc4343d accidentally inverted
the logic, returning false when the error was an exec.ExitError.

pkg/runner/libfuzzer/libfuzzer_runner.go
@@ -337,7 +337,7 @@ func IsExpectedExitError(err error) bool {
 	}
 
 	var exitErr *exec.ExitError
-	if errors.As(err, &exitErr) {
+	if !errors.As(err, &exitErr) {
 		return false
 	}
 	return sliceutil.Contains(expectedExitCodes, exitErr.ExitCode())